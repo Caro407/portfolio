c6d313700f9160bce02f8c8d52297103f6d55d32
Caro407 <caro.lefevre407@hotmail.fr>
2021-07-17 18:12:08 +0200

    add contact form component

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactForm = () => {
+
+  return (
+    <form>
+      <label>Mets le texte ici</label>
+    </form>
+  )
+};
+
+export default ContactForm;
--- src/pages/sections/Contact/index.jsx
+++ src/pages/sections/Contact/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactForm from './../../../components/ContactForm';
 
 const Contact = () => {
 
@@ -7,6 +8,12 @@ const Contact = () => {
       <h2 className="font-bold text-xl m-2">Contact</h2>
       <div className="divider m-auto w-64 h-2 mb-3"></div>
       <p>Interested ? Let's get in touch</p>
+      <div>
+        <button className="border rounded-md px-2 py-1">View resume</button>
+        <br />
+        <a href="https://www.linkedin.com/in/caroline-lefevre-a2512794/" className="border rounded-md px-2 py-1">View LinkedIn</a>
+      </div>
+      <ContactForm />
     </section>
   )
 };
