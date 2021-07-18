99e42e0dea91cb2a4b0b541a3e5e81c69f34b76b
Caro407 <caro.lefevre407@hotmail.fr>
2021-07-18 14:03:45 +0200

    add icon for link on preview

--- src/components/ProjectPreview/index.jsx
+++ src/components/ProjectPreview/index.jsx
@@ -6,9 +6,12 @@ const ProjectPreview = (props) => {
     <div className="border rounded m-1 px-2 py-3">
       <Link to={`/projects/${props.project.slug}`} className="font-bold">{props.project.title}</Link>
       <a href={props.project.github[0]}><div className="github-link"></div></a>
+      <a href={props.project.website_link}>
+        <i className="fas fa-link"></i>
+      </a>
       <details>
         <summary>Technologies used</summary>
-        {props.project.frameworks.map(framework => <li>{framework.name}</li>)}
+        {props.project.frameworks.map((framework, index) => <li key={`${framework.name}-${index}`}>{framework.name}</li>)}
       </details>
       <p className="text-sm italic py-2">{props.project.pitch}</p>
     </div>
