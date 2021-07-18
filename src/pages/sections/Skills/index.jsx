13b64e23d8230c62c545312a8bb5e4e44ddfaa34
Caro407 <caro.lefevre407@hotmail.fr>
2021-07-18 14:44:42 +0200

    add progress bars

--- src/pages/sections/Skills/index.jsx
+++ src/pages/sections/Skills/index.jsx
@@ -6,13 +6,26 @@ const Skills = () => {
       <h2 className="font-bold text-xl m-2">Skills</h2>
       <div className="divider m-auto w-64 h-2 mb-3"></div>
       <p>J'aime bien le JS pour faire du React mais aussi le Ruby quand même.</p>
-      <ul className="list-disc text-left">
-        <li>React & Next.js</li>
-        <li>Ruby on Rails</li>
-        <li>JS (ES5/ES6)</li>
-        <li>HTML 5 / CSS 3 / SCSS</li>
-        <li>Bootstrap / Tailwind.css</li>
-      </ul>
+      <div className="block text-left">
+        <label for="react">React & Next.js</label>
+        <progress id="react" max="100" value="80"> 80% </progress>
+      </div>
+      <div className="block text-left">
+        <label for="rails">Ruby on Rails</label>
+        <progress id="rails" max="100" value="70"> 70% </progress>
+      </div>
+      <div className="block text-left">
+        <label for="js">JS (ES5/ES6)</label>
+        <progress id="js" max="100" value="80"> 80% </progress>
+      </div>
+      <div className="block text-left">
+        <label for="front-languages">HTML 5 / CSS 3 / SCSS</label>
+        <progress id="front-languages" max="100" value="60"> 60% </progress>
+      </div>
+      <div className="block text-left">
+        <label for="front-frameworks">Bootstrap / Tailwind.css</label>
+        <progress id="front-frameworks" max="100" value="80"> 80% </progress>
+      </div>
     </section>
   )
 };
