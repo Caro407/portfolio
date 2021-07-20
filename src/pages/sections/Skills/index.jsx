57b7a196cec37daaca238170d5f292d8a4cb2014
Caro407 <caro.lefevre407@hotmail.fr>
2021-07-20 17:42:10 +0200

    add animation on progress bars

--- src/pages/sections/Skills/index.jsx
+++ src/pages/sections/Skills/index.jsx
@@ -6,26 +6,28 @@ const Skills = () => {
       <h2 className="font-bold text-xl m-2">Skills</h2>
       <div className="divider m-auto w-64 h-2 mb-3"></div>
       <p>J'aime bien le JS pour faire du React mais aussi le Ruby quand même.</p>
-      <div className="block text-left">
-        <label for="react">React & Next.js</label>
-        <progress id="react" max="100" value="80"> 80% </progress>
+
+      <label for="front-frameworks" className="block text-left">React & Next.js</label>
+      <div className="progress">
+        <span className="progress-bar" style={{width: 85+"%"}}></span>
       </div>
-      <div className="block text-left">
-        <label for="rails">Ruby on Rails</label>
-        <progress id="rails" max="100" value="70"> 70% </progress>
+      <label for="front-frameworks" className="block text-left">Ruby on Rails</label>
+      <div className="progress">
+        <span className="progress-bar" style={{width: 70+"%"}}></span>
       </div>
-      <div className="block text-left">
-        <label for="js">JS (ES5/ES6)</label>
-        <progress id="js" max="100" value="80"> 80% </progress>
+      <label for="front-frameworks" className="block text-left">JS (ES5/ES6)</label>
+      <div className="progress">
+        <span className="progress-bar" style={{width: 80+"%"}}></span>
       </div>
-      <div className="block text-left">
-        <label for="front-languages">HTML 5 / CSS 3 / SCSS</label>
-        <progress id="front-languages" max="100" value="60"> 60% </progress>
+      <label for="front-frameworks" className="block text-left">HTML 5 / CSS 3 / SCSS</label>
+      <div className="progress">
+        <span className="progress-bar" style={{width: 75+"%"}}></span>
       </div>
-      <div className="block text-left">
-        <label for="front-frameworks">Bootstrap / Tailwind.css</label>
-        <progress id="front-frameworks" max="100" value="80"> 80% </progress>
+      <label for="front-frameworks" className="block text-left">Bootstrap / Tailwind.css</label>
+      <div className="progress">
+        <span className="progress-bar" style={{width: 80+"%"}}></span>
       </div>
+
     </section>
   )
 };
