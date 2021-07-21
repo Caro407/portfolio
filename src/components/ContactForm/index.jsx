42f9d299e1fef00853ad126305de2e67720f4f1d
Caro407 <caro.lefevre407@hotmail.fr>
2021-07-21 16:20:27 +0200

    fix front on home

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -5,10 +5,10 @@ const ContactForm = () => {
   return (
     <form>
       <div className="block my-1">
-        <input type="text" placeholder="Subject" className="w-3/5 px-2 rounded-sm"/>
+        <input type="text" placeholder="Subject" className="w-4/5 px-2 rounded-sm"/>
       </div>
       <div className="block my-1 h-1/4">
-        <textarea type="textarea" placeholder="Say hello !" cols="50" rows="4" className="w-3/5 px-2 rounded-sm h-full"></textarea>
+        <textarea type="textarea" placeholder="Say hello !" cols="50" rows="4" className="w-4/5 px-2 rounded-sm h-full"></textarea>
       </div>
       <button type="submit" className="sendBtn">Send</button>
     </form>
